fdf441744ead6e56885112c82da85e5a726250e4
Gilad Ben-Yossef <gilad@benyossef.com>
2013-06-26 17:24:59 +0100

    mm: make vmstat_update periodic run conditional
    vmstat_update runs every second from the work queue to update statistics
    and drain per cpu pages back into the global page allocator.
    
    This is useful in most circumstances but is wasteful if the CPU doesn't
    actually make any VM activity. This can happen in the situtation that
    the CPU is idle or running a CPU bound long term task (e.g. CPU
    isolation), in which case the periodic vmstate_update timer needlessly
    itnerrupts the CPU.
    
    This patch tries to make vmstat_update schedule itself for the next
    round only if there was any work for it to do in the previous run.
    The assumption is that if for a whole second we didn't see any VM
    activity it is reasnoable to assume that the CPU is not using the
    VM because it is idle or runs a long term single CPU bound task.
    
    A new single unbound system work queue item is scheduled periodically
    to monitor CPUs that have their vmstat_update work stopped and
    re-schedule them if VM activity is detected.
    
    Signed-off-by: Gilad Ben-Yossef <gilad@benyossef.com>
    CC: Thomas Gleixner <tglx@linutronix.de>
    CC: Tejun Heo <tj@kernel.org>
    CC: John Stultz <johnstul@us.ibm.com>
    CC: Andrew Morton <akpm@linux-foundation.org>
    CC: KOSAKI Motohiro <kosaki.motohiro@jp.fujitsu.com>
    CC: Mel Gorman <mel@csn.ul.ie>
    CC: Mike Frysinger <vapier@gentoo.org>
    CC: David Rientjes <rientjes@google.com>
    CC: Hugh Dickins <hughd@google.com>
    CC: Minchan Kim <minchan.kim@gmail.com>
    CC: Konstantin Khlebnikov <khlebnikov@openvz.org>
    CC: Christoph Lameter <cl@linux.com>
    CC: Chris Metcalf <cmetcalf@tilera.com>
    CC: Hakan Akkan <hakanakkan@gmail.com>
    CC: Max Krasnyansky <maxk@qualcomm.com>
    CC: Frederic Weisbecker <fweisbec@gmail.com>
    CC: linux-kernel@vger.kernel.org
    CC: linux-mm@kvack.org

--- include/linux/vmstat.h
+++ include/linux/vmstat.h
@@ -199,7 +199,7 @@ extern void __inc_zone_state(struct zone *, enum zone_stat_item);
 extern void dec_zone_state(struct zone *, enum zone_stat_item);
 extern void __dec_zone_state(struct zone *, enum zone_stat_item);
 
-void refresh_cpu_vm_stats(int);
+bool refresh_cpu_vm_stats(int);
 void refresh_zone_stat_thresholds(void);
 
 int calculate_pressure_threshold(struct zone *zone);
