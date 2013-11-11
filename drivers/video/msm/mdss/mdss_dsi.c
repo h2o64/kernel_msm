f9f3f5b04ef4a4b1a52e04d2cc507e9190310f23
Konstantin Makariev <hcv867@motorola.com>
2013-11-11 11:19:48 -0600

    msm: mdss: MMI panel notifications
    This is power safe alternative to frame buffer notifications.
    Panels with in-cell touch solution demand power supplied without
    interruptions. Frame buffer notifications cannot fulfil this
    requirement. MMI panel notifications can.
    
    [h2o64] Added some comments ro figure out the ESD implementation
    
    Change-Id: Ica3192b794665031c9fafb3e45bd156de62226eb
    Signed-off-by: Konstantin Makariev <hcv867@motorola.com>
    Signed-off-by: Zac Crosby <azc095@motorola.com>

--- drivers/video/msm/mdss/mdss_dsi.c
+++ drivers/video/msm/mdss/mdss_dsi.c
@@ -870,6 +870,9 @@ int mdss_dsi_cont_splash_on(struct mdss_panel_data *pdata)
 	mdss_dsi_sw_reset(pdata);
 	mdss_dsi_host_init(pdata);
 	mdss_dsi_op_mode_config(mipi->mode, pdata);
+
+//	if (ctrl_pdata->cont_splash_on)
+//		ctrl_pdata->cont_splash_on(pdata);
 	pr_debug("%s-:End\n", __func__);
 	return ret;
 }
