7576f08c7ff1c29db3aa1e312129937afcd1ce15
Shengzhe Zhao <a18689@motorola.com>
2014-06-06 14:29:31 -0700

    msm: mdss: provide display panel info in sysfs
    Provide display panel information including panel name, panel version
    manufacture id, controller version, controller drv version under
    /sys/class/graphics/fb0.
    
    Port of 48a13a7.
    
    Change-Id: I036b38ca122ae7eeae406ed31e3ba7fa06b74fe7
    Signed-off-by: Shengzhe Zhao <a18689@motorola.com>
    Signed-off-by: Bang Nguyen <bnguyen1@motorola.com>
    Signed-off-by: Zac Crosby <zac@motorola.com>
    Reviewed-on: http://gerrit.mot.com/653179
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Reviewed-by: Joseph Swantek <jswantek@motorola.com>

--- drivers/video/msm/mdss/mdss_fb.h
+++ drivers/video/msm/mdss/mdss_fb.h
@@ -238,6 +238,13 @@ struct msm_fb_data_type {
 	struct ion_handle *fb_ion_handle;
 };
 
+struct sys_panelinfo {
+	char *panel_name;
+	u64 *panel_ver;
+};
+
+extern struct sys_panelinfo panelinfo;
+
 static inline void mdss_fb_update_notify_update(struct msm_fb_data_type *mfd)
 {
 	int needs_complete = 0;
