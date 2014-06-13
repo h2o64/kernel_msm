14eeb09c8e7aab6e1bc33f00a418ebd57a332e15
Bang Nguyen <bnguyen1@motorola.com>
2014-06-13 15:51:53 -0500

    msm: mdss: Support disp_util tool
    Support the disp_util tool to set and get the register information from
    MIPI DSI display from user space
    
    Change-Id: If0b8fbcded6059664d595a74393577d536e199d2
    Signed-off-by: Bang Nguyen <bnguyen1@motorola.com>

--- include/linux/msm_mdp.h
+++ include/linux/msm_mdp.h
@@ -81,6 +81,8 @@
 #define MSMFB_ASYNC_BLIT              _IOW(MSMFB_IOCTL_MAGIC, 168, unsigned int)
 #define MSMFB_OVERLAY_PREPARE		_IOWR(MSMFB_IOCTL_MAGIC, 169, \
 						struct mdp_overlay_list)
+#define MSMFB_REG_READ   _IOWR(MSMFB_IOCTL_MAGIC, 64, struct msmfb_reg_access)
+#define MSMFB_REG_WRITE  _IOW(MSMFB_IOCTL_MAGIC, 65, struct msmfb_reg_access)
 #define MSMFB_LPM_ENABLE        _IOWR(MSMFB_IOCTL_MAGIC, 170, unsigned int)
 #define FB_TYPE_3D_PANEL 0x10101010
 #define MDP_IMGTYPE2_START 0x10000
@@ -1104,6 +1106,13 @@ struct msmfb_mixer_info_req {
 	int mixer_num;
 	int cnt;
 	struct mdp_mixer_info info[MAX_PIPE_PER_MIXER];
+};
+
+struct msmfb_reg_access {
+	uint8_t address;
+	uint8_t use_hs_mode;
+	size_t buffer_size;
+	void __user *buffer;
 };
 
 enum {
