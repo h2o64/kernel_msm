c603835898d865b8e3da668055ebfb1e227a6654
Zac Crosby <zac@motorola.com>
2014-10-17 15:56:51 -0500

    msm: mdss: fix race condition of DSI off and status check.
    fb_event_callback() is what is responsible for cancelling the delayed_work
    when it gets the FB_BLANK_POWERDOWN event. However, this event occurs after
    the panel & DSI have been turned off. So if the check_status function was
    being called right when the panel is being turned off, we have a race
    conditon. Hence Add offlock mutex to ensure that bta status check is done
    when dsi is being turned off.
    
    Port of 6a6338704
    
    Signed-off-by: Zac Crosby <zac@motorola.com>
    Change-Id: Ic0bc5a2734472a143e35c0701d93ff1f34b1ed9a
    Reviewed-on: http://gerrit.mot.com/685417
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Shi-Yong Li <a22381@motorola.com>
    Reviewed-by: Bang Nguyen <bangnguyen@motorola.com>
    Reviewed-by: Stephen Rossbach <rossbach@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/video/msm/mdss/mdss_mdp_intf_video.c
+++ drivers/video/msm/mdss/mdss_mdp_intf_video.c
@@ -317,11 +317,13 @@ static int mdss_mdp_video_stop(struct mdss_mdp_ctl *ctl)
 		return -ENODEV;
 	}
 	MDSS_XLOG(ctl->num, ctl->vsync_cnt);
+	mutex_lock(&ctl->offlock);
 	if (ctx->timegen_en) {
 		rc = mdss_mdp_ctl_intf_event(ctl, MDSS_EVENT_BLANK, NULL);
 		if (rc == -EBUSY) {
 			pr_debug("intf #%d busy don't turn off\n",
 				 ctl->intf_num);
+			mutex_unlock(&ctl->offlock);
 			return rc;
 		}
 		WARN(rc, "intf %d blank error (%d)\n", ctl->intf_num, rc);
@@ -361,6 +363,7 @@ static int mdss_mdp_video_stop(struct mdss_mdp_ctl *ctl)
 	mdss_mdp_set_intr_callback(MDSS_MDP_IRQ_INTF_UNDER_RUN, ctl->intf_num,
 				   NULL, NULL);
 
+	mutex_unlock(&ctl->offlock);
 	mdss_mdp_ctl_reset(ctl);
 	ctx->ref_cnt--;
 	ctl->priv_data = NULL;
