9961f7178fc0a8502c21d0122abdbc9c508747b2
desaishivam26 <kane.desai@gmail.com>
2015-08-07 14:34:54 +0200

    cpufreq: smartmax: adapt for falcon

--- drivers/cpufreq/cpufreq_smartmax.c
+++ drivers/cpufreq/cpufreq_smartmax.c
@@ -53,7 +53,7 @@
  * lowering the frequency towards the ideal frequency is faster than below it.
  */
 
-#define GOV_IDLE_FREQ 475000
+#define GOV_IDLE_FREQ 384000
 
 #define DEFAULT_SUSPEND_IDEAL_FREQ GOV_IDLE_FREQ
 static unsigned int suspend_ideal_freq;
@@ -111,7 +111,7 @@ static unsigned int sampling_rate;
 #define DEFAULT_INPUT_BOOST_DURATION 50000000
 static unsigned int input_boost_duration;
 
-static unsigned int touch_poke_freq = 760000;
+static unsigned int touch_poke_freq = 787200;
 static bool touch_poke = true;
 
 /*
@@ -123,7 +123,7 @@ static bool ramp_up_during_boost = true;
  * external boost interface - boost if duration is written
  * to sysfs for boost_duration
  */
-static unsigned int boost_freq = 760000;
+static unsigned int boost_freq = 787200;
 static bool boost = true;
 
 /* in nsecs */
